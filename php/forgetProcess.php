2967d86c3636b027279ad81f6386fc12eef7bea7
Md. Shorifuzzaman <shohan.jess@gmail.com>
2017-03-15 01:55:37 +0600

    login

---
+++ php/forgetProcess.php
@@ -0,0 +1,17 @@
+<?php
+	$email =$_POST['email'];
+	$connection= mysqli_connect("localhost", "root", "abcd");
+	mysqli_select_db($connection,"login");
+	$result = mysqli_query($connection,"select * from users where email='$email'") or die("Failed to query database ".mysqli_error($connection));
+	$row =mysqli_fetch_array($result);
+	if($row['email']==$email){
+		$to = $email;
+		$subject = "Picard account password reset";
+		$txt = "Picard account:\n\t Email: ".$email."\n\t Password: ".$row['password'];
+		$headers = "From: shohan.jess@gmail.com" . "\r\n";
+		mail($to,$subject,$txt,$headers);
+	}
+	else{
+		echo "failed";
+	}
+?>
---
+++ php/registrationProcess.php
@@ -0,0 +1,17 @@
+<?php
+	if(isset($_POST['submit'])){
+	$email =$_POST['email'];
+	$password = $_POST['password'];
+	$connection= mysqli_connect("localhost", "root", "abcd");
+	mysqli_select_db($connection,"login");
+	$result = mysqli_query($connection,"select * from users where email='$email'") or die("Failed to query database ".mysqli_error($connection));
+	$row =mysqli_fetch_array($result);
+	if($row['email']==$email){
+		echo "Already have an account with this mail";
+	}
+	else{
+	$result = mysqli_query($connection,"insert into users(email,password) values('$email','$password')") or die("Failed to query database ".mysqli_error($connection));
+		header("location: ../login/login.php");
+	}
+}
+?>
