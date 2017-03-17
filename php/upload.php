84c9d956d48371c15153a80b3439099f3e78fbeb
Azharuddin Ruhit <ruhit1011@gmail.com>
2017-03-17 17:39:37 +0600

    Update upload.php

--- php/upload.php
+++ php/upload.php
@@ -1,13 +1,16 @@
 <!DOCTYPE html>
 <!DOCTYPE html>
 <html>
-<head>
-	<title>Image upload</title>
-</head>
-<body>
-<form action="uploadProcess.php" method="POST" enctype="multipart/form-data">
-	<input type="file" name="files[]" multiple accept='image/*'> 
-	<input type="submit" name="upload" value="Upload">
- </form>
-</body>
+    <head>
+        <title>Image upload</title>
+        <link rel="stylesheet" type="text/css" href="../css/upload.css">
+    </head>
+    <body>
+        <div class="center">
+            <form action="uploadProcess.php" method="POST" enctype="multipart/form-data">
+                <input type="file" name="files[]" multiple accept='image/*'> 
+                <input type="submit" name="upload" value="Upload">
+            </form>
+        </div>
+    </body>
 </html>
